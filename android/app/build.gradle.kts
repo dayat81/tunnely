81208f56d2dff68b4411781e6345b49eae693bc8
dayat81 <dayat81@users.noreply.github.com>
2026-06-28 12:34:05 +0700

    chore: bump version to 1.3.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tunnely.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.2"
+        versionCode = 9
+        versionName = "1.3.3"
     }
 
     buildTypes {
