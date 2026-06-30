4db4d666276d8b365396e74da068eb867c06f359
dayat81 <dayat81@users.noreply.github.com>
2026-06-30 23:33:57 +0700

    chore: bump to v3.20.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tunnely.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 91
-        versionName = "3.20.1"
+        versionCode = 92
+        versionName = "3.20.2"
     }
 
     signingConfigs {
